cab7c71f3bb7e6e16b0d4d652a4e7b8583cd2950
EMIRATES\S434647 <sreejith.sreekumar@emirates.com>
2018-06-04 23:32:31 +0400

    first ng5 app

---
+++ src/app/data.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+	private goals = new BehaviorSubject<any>(['goal1', 'goal2']);
+	goal = this.goals.asObservable();
+
+  	constructor() { }
+
+  	changeGoal(goal) {
+  		this.goal.next(goal);
+  	}
+}
